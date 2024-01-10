520e8d7a214dc8f02de73582f0dd88737ee0209f
Charles <Charlessammarco@gmail.com>
2024-01-09 20:21:30 -0600

    oxford

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        last_item = items.pop(-1)
+        x =", ".join(items)
+        return f"{x}, and {last_item}"
